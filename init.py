a35d9e23558ca1b3b2b5f1c871c78c80e3ea2c47
Philip Sahli <philip@sahli.net>
2018-08-10 07:52:41 +0200

    change port, add configure_nginx to init

--- init.py
+++ init.py
@@ -1,4 +1,5 @@
 def func(self):
-    self.siblings.install_boto(self)
-    self.siblings.install_main_module(self)
+    self.siblings.configure_nginx(self)
+    # self.siblings.install_boto(self)
+    # self.siblings.install_main_module(self)
     return "INSTALLED"
