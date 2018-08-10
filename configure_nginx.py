a9914bde4fcb99e9abdaa999ea89e23fb48b09c5
Philip Sahli <philip@sahli.net>
2018-08-10 07:42:33 +0200

    redirect of j2 output

--- configure_nginx.py
+++ configure_nginx.py
@@ -5,13 +5,15 @@ def func(self):
 
     # get config
     import urllib
-    urllib.urlretrieve ("https://raw.githubusercontent.com/sahlinet/tumbo-io/develop/files/default.conf", "/etc/nginx/conf.d/default.conf")
+    urllib.urlretrieve ("https://raw.githubusercontent.com/sahlinet/tumbo-io/develop/files/default.conf", "/tmp/default.conf.j2")
+    h = open("/etc/nginx/conf.d/default.conf.j2", "a")
 
     # render config
     import sh
     self.pip_install("j2cli")
     j2 = sh.Command("j2")
-    j2("/etc/nginx/conf.d/default.conf")
+
+    j2("/tmp/default.conf.j2", _out=h)
 
     # start nginx
     nginx = sh.Command("nginx")
